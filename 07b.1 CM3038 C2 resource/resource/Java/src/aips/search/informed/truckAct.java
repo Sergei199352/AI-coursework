7d11475477d546bbcbfd89bf53b5bdd531e647a9
Sergei199352 <s.stanislavchuk@rgu.ac.uk>
2023-03-12 11:07:48 +0000

    continuing on the heuristic

---
+++ 07b.1 CM3038 C2 resource/resource/Java/src/aips/search/informed/truckAct.java
@@ -0,0 +1,23 @@
+package aips.search.informed;
+
+import aips.search.Action;
+
+public class truckAct extends Action {
+
+
+   public double cargo_am; // the amount of cargo moved
+
+   public int from_city; // which sity it comes from
+
+   public int to_city; // which city it goes to
+
+
+
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
+    
+}
